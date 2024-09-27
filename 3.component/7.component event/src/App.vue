51eb900ee4ef39c9d3fa59ae653123ccfe4e4be5
黃耀德 <yaote@huangyaodedeMacBook-Pro.local>
2024-09-27 17:12:37 +0800

    update

---
+++ 3.component/7.component event/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <dev>
+      <BlogPostItem v-for="item in postitems"
+        :key="item.id"
+        :id="item.id"
+        :title="item.title"
+        @deletePost="handleDeletePost"/>
+    </dev>
+  </main>
+</template>
+<script>
+  import BlogPostItem from './components/BlogPostItem.vue'
+
+  export default {
+    components: {
+      BlogPostItem
+    },
+    data() {
+      return {
+        postitems: [
+          {
+            id: 1,
+            title: "Post 1"
+          },
+          {
+            id: 2,
+            title: "Post 2"
+          },
+          {
+            id: 3,
+            title: "Post 3"
+          }
+        ]
+      };
+    },
+    methods: {
+      handleDeletePost(id) {
+        this.postitems = this.postitems.filter((p) => p.id != id);
+      }
+    }
+  }
+</script>
+<style scoped>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
+</style>
---
+++ 3.component/7.component event/src/components/BlogPostItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <a>{{ title }}</a>
+        <button @click="$emit('deletePost', id)">刪除</button>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            id: Number,
+            title: String
+        },
+        emits: ["deletePost"]
+    }
+</script>
+<style scoped>
+    div {
+        margin-bottom: 1em;
+    }
+
+    button {
+        margin-left: 1em;
+        padding: 0.5em 1em;
+        color: white;
+        border: none;
+        background: linear-gradient(90deg, hsl(240deg, 50%, 50%), hsl(280deg, 50%, 50%));
+    }
+</style>
